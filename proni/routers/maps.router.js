f884ba8a16658dcf3db1331d7c7ce1c021f07529
Belmont <jisagiizacc@gmail.com>
2023-11-19 17:58:43 -0600

    Quitar console.log

--- proni/routers/maps.router.js
+++ proni/routers/maps.router.js
@@ -52,7 +52,6 @@ router.get('/proni/general/:tipo', async (req, res, next) => {
         next(resultado);
         return;
     }
-    console.log(resultado.titulo);
     res.render('informe_general.ejs', {
         imagenes: resultado.imagenes, tablas: resultado.tablas, cabezeras: resultado.cabezeras,
         titulo: resultado.titulo, tipo: resultado.tipo, linkTablas: resultado.linkTablas
